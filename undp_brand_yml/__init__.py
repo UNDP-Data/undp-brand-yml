b05f6bc0ebfa1e8354a35ca79b8153cf48084348
Mykola Skrynnyk <45297092+mykolaskrynnyk@users.noreply.github.com>
2025-07-16 19:10:05 +0200

    Feat(src): add and expose a basic `brand.yml` file

--- undp_brand_yml/__init__.py
+++ undp_brand_yml/__init__.py
@@ -2,3 +2,11 @@
 A utility package that provides `brand.yml` definition for applying
 UNDP-branded styling in Shiny and Quarto projects.
 """
+
+from importlib import resources
+
+from brand_yml import Brand
+
+__all__ = ["brand"]
+
+brand = Brand.from_yaml(resources.files().joinpath("_brand.yml"))
